214edaafb70887d47e9a40d07abfd68fceeec581
elie.kazan <elie.kazan@ens.uvsq.fr>
2021-03-18 15:05:16 +0100

    coordonne reel

--- Vision/ball.py
+++ Vision/ball.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import cv2
 import numpy as np
+import time
 
 WINDOW_NAME = 'BallTracker' 
 
@@ -38,12 +39,13 @@ def track(image,color):
 
     # On brouille le masque pour distinguer plus facilement les patternes --- Application du masque permettant grâce à l'extérieur de l'objet de le distinguer plus clairement
     bmask = cv2.GaussianBlur(mask, (5,5),0)
-
+    cv2.imshow("bmask",bmask)
     # On trouve le centre de l'objet
     moments = cv2.moments(bmask)
     m00 = moments['m00']
     centroid_x, centroid_y = None, None
     if m00 != 0:
+
         centroid_x = int(moments['m10']/m00)
         centroid_y = int(moments['m01']/m00)
 
@@ -53,7 +55,8 @@ def track(image,color):
     # Pointe l'objet si on a des coordonnées non nulles sur x et y
     if centroid_x != None and centroid_y != None:
         #Ctr : coordonnée centre objet
-        ctr = (centroid_x, centroid_y)
+        ctr = (centroid_x,centroid_y)
+
 
         # Créer un pointeur noir
         cv2.circle(image, ctr, 4, (0,0,0))
@@ -80,8 +83,11 @@ while True:
     okay, image = capture.read()    #Détecte si le flux de caméra existe
 
     if okay:
-        print(track(image,color))         #Si un flux de caméra existe, on utilise track (on trouve le centre de l'objet)
-
+        
+        ctr = track(image,color)         #Si un flux de caméra existe, on utilise track (on trouve le centre de l'objet)
+        x = ctr[0]
+        y = ctr[1]
+        print(int(x/7) , int(y/7))
         if not track(image,color):
             break
 
